72c8800f80738ec68f2aa4038838265f32dce1d8
hisyamyasidp <hisyamyasidp@gmail.com>
2025-09-17 16:15:06 +0700

    refactor: ubah main.py jadi ada input nama

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Halo GitHub dari Python!")
+def main():
+    print("=== Program Nyoba GitHub ===")
+    nama = input("Masukkan nama kamu: ")
+    print(f"Halo, {nama}! Selamat datang di repo NyobaGit 🚀")
+
+if __name__ == "__main__":
+    main()
